b5b3e767b39104381cdb61ac38f6b68eb637f0a9
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-02-09 16:26:35 -0300

    Downloader Interface and Directory Save

--- downloader.py
+++ downloader.py
@@ -1,64 +1,50 @@
 import tkinter as tk
+from tkinter import filedialog, messagebox
 from pytube import YouTube
 import os
 
-# window = tk.Tk()
-# window.title('Youtube Downloader')
-# window.geometry('500x300')
+def downloader_mp3():
+    url = entry.get()
+    yt = YouTube(url)
+    video_title = yt.title
 
-url = input("Enter the url of the video: ")
-yt = YouTube(url)
-video_title = yt.title
+    save_path = filedialog.askdirectory()
+    if save_path:
+        print(f"Baixando MP3 do vídeo '{video_title}'...")
+        audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        audio_file = audio_stream.download(output_path=save_path, filename='audio_only')
+        base, ext = os.path.splitext(audio_file)
+        new_file = f"{video_title}.mp3"  # Usando o título do vídeo como nome do arquivo
+        os.rename(audio_file, os.path.join(save_path, new_file))
+        messagebox.showinfo("Download Concluído", "Download do MP3 concluído!")
 
-print("Baixar: ")
-print("1. MP3")
-print("2. MP4")
+def downloader_mp4():
+    url = entry.get()
+    yt = YouTube(url)
+    video_title = yt.title
 
-option = int(input("Option: "))
-
-if option == 1:
-    print(f"Baixando MP3 do vídeo '{video_title}'...")
-    audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
-    audio_file = audio_stream.download(output_path=".", filename='audio_only')
-    base, ext = os.path.splitext(audio_file)
-    new_file = f"{video_title}.mp3"  # Usando o título do vídeo como nome do arquivo
-    os.rename(audio_file, new_file)
-    print("Download concluído!")
-
-elif option == 2:
-    print("Qualidade: ")
-    print("1. 360p")
-    print("2. 720p")
-    print("3. 1080p")
-
-    quality = int(input("Option: "))
-
-    if quality == 1:
-        print(f"Baixando MP4 em 360p do vídeo '{video_title}'...")
-        yt.streams.filter(res="360p").first().download()
-        new_file = f"{video_title}.mp4"  # Usando o título do vídeo como nome do arquivo
-        os.rename(video_title + ".mp4", new_file)
-        print("Download concluído!")
-
-    elif quality == 2:
-        print("Baixando MP4 em 720p do vídeo '{video_title}'...")
-        yt.streams.filter(res="720p").first().download()
+    save_path = filedialog.askdirectory()
+    if save_path:
+        print(f"Baixando em MP4 do vídeo '{video_title}'...")
+        yt.streams.filter(res="720p").first().download(output_path=save_path)
         new_file = f"{video_title}.mp4"
-        os.rename(video_title + ".mp4", new_file)
-        print("Download concluído!")
+        os.rename(os.path.join(save_path, video_title + ".mp4"), os.path.join(save_path, new_file))
+        messagebox.showinfo("Download Concluído", "Download do MP4 concluído!")
 
-    elif quality == 3:
-        print("Baixando MP4 em 1080p do vídeo '{video_title}'...")
-        yt.streams.filter(res="1080p").first().download()
-        new_file = f"{video_title}.mp4"
-        os.rename(video_title + ".mp4", new_file)
-        print("Download concluído!")
+window = tk.Tk()
+window.title('Youtube Downloader')
+window.geometry('500x200')
 
-    else:
-        print("Opção inválida!")
+label = tk.Label(window, text='Youtube Downloader', font=("Comic Sans MS", 20, "bold"))
+label.pack()
 
-else:
-    print("Opção inválida!")
+entry = tk.Entry(window, width=50, font=('Arial', 14))
+entry.pack()
 
+button = tk.Button(window, text='Download MP3', command=downloader_mp3, font=("Comic Sans MS", 10, "bold"))
+button.pack()
 
-#window.mainloop()
+button2 = tk.Button(window, text='Download MP4', command=downloader_mp4, font=("Comic Sans MS", 10, "bold"))
+button2.pack()
+
+window.mainloop()
